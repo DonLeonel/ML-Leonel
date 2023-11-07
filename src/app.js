40d077a89f6dc20f1943d745dd21a0b446dff6ee
DonLeonel <leonel.don@hotmail.es>
2023-11-07 14:57:30 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+
+app.listen(3000, ()=> console.log('Escuchando en puerto 3000'))
