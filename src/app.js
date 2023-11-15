9dbdf79236faa4ed15e3faf271f2b925943ffb25
DonLeonel <leonel.don@hotmail.es>
2023-11-15 15:17:38 -0300

    Correciones y agregado de login

--- src/app.js
+++ src/app.js
@@ -13,6 +13,10 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/register.html'))
 })
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+
 
 const _PORT = process.env.PORT || 3001;
 app.listen(_PORT, ()=> console.log('Servidor corriendo en http://localhost:'+_PORT+'/'))
